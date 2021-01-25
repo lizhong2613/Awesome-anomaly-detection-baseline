83d91d1ec04554cce670ddd01aeff0365bc315cd
LinghaoChan <Linghao_Chan@163.com>
2021-01-25 09:51:12 +0800

    code

---
+++ Radar/radar.m
@@ -0,0 +1,25 @@
+function [R] = radar(X, A, L, alpha, beta, gamma, niters)
+
+    [n,d] = size(X);
+    Dr = eye(n);
+    Dw = eye(n);
+    R = inv(eye(n)+beta*Dr+gamma*L)*X;
+    for iter = 1:niters
+        %% update W
+        W = inv(X*X'+alpha*Dw)*(X*X'-X*R');
+        Wtmp = sqrt(sum(W.*W,2)+eps);
+        Dw = diag(0.5./Wtmp);
+        
+        %% update R
+        R = inv(eye(n)+beta*Dr+gamma*L)*(X-W'*X);
+        Rtmp = sqrt(sum(R.*R,2)+eps);
+        Dr = diag(0.5./Rtmp);
+        
+        %% check if the objective function converges
+        obj(iter) = norm(X-W'*X-R,'fro')^2 + alpha*sum(Wtmp) + beta*sum(Rtmp) + gamma*trace(R'*L*R);
+        fprintf('the object value in iter %d is %f\n', iter, obj(iter));
+        if (iter >= 2 && abs(obj(iter)-obj(iter-1))<1e-3)
+            break;
+        end
+    end
+end
---
+++ Radar/run.m
@@ -0,0 +1,22 @@
+load data/DBLP.mat;
+
+[n,~] = size(X);
+X = normalizeFea(X, 0);
+niters = 10;
+
+alpha = 0.1;
+beta = 0.01;
+gamma = 0.1;
+At = A';
+Anew = max(A,At);
+L = computelaplacian(Anew, 'undirected');
+R = radar(X, A, L, alpha, beta, gamma, niters);
+score= sum(R.*R,2);
+[~,idx] = sort(score, 'descend');
+
+gnd_data = zeros(n,2);
+gnd_data(:,1) = gnd;
+gnd_data(:,2) = score;
+[tp,fp] = roc([gnd_data(:,1),gnd_data(:,2)]);
+plot(fp,tp);
+auc_value = auc(gnd_data);
