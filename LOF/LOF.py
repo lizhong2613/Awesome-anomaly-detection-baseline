6e0f2e94c584db37003149cecffc5c974c3152fb
LinghaoChan <Linghao_Chan@163.com>
2021-01-30 21:28:36 +0800

    precisionatk

--- LOF/LOF.py
+++ LOF/LOF.py
@@ -3,6 +3,7 @@ from sklearn.neighbors import LocalOutlierFactor
 import calculateAUC
 import getData
 from recallAtK import calculateRecallAtK
+from precisionAtK import calculatePrecisionAtK
 
 def main(K:int):
     data_generator = getData.GetData('data/Amazon.mat')
@@ -14,9 +15,10 @@ def main(K:int):
     anom_score = -(lof.negative_outlier_factor_)
 
     RecallatK = calculateRecallAtK(anom_score, gnd, K)
-    print("Recall @ {}: \t{}".format(K, RecallatK))
-
-    print("AUC value: \t{}".format(calculateAUC.getAUC(score=anom_score, gnd=gnd)))
+    PrecisionatK = calculatePrecisionAtK(anom_score, gnd, K)
+    print("Recall@ {}: \t\t{}".format(K, RecallatK))
+    print("Precision@ {}: \t{}".format(K, PrecisionatK))
+    print("AUC value: \t\t{}".format(calculateAUC.getAUC(score=anom_score, gnd=gnd)))
 
 if __name__ == "__main__":
     main(300)
